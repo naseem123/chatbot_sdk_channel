87dd24ebd45229ad9397795fe80d93bbb6b22a4b
naseemzyngl <naseem@zartek.in>
2024-02-26 01:11:19 +0530

    Minor Update

--- lib/features/chatbot/presentation/chat_home/widgets/chat_home_appbar.dart
+++ lib/features/chatbot/presentation/chat_home/widgets/chat_home_appbar.dart
@@ -38,8 +38,7 @@ class ChatBotAppbar extends StatelessWidget implements PreferredSizeWidget {
               ),
               trailing: IconButton(
                 onPressed: () {
-                  //TODO(naseem) : need to exit from the bot once SDK setup is complete
-                  // Navigator.of(context).pop();
+                   Navigator.of(context).pop();
                 },
                 icon: const Icon(
                   Icons.close,
