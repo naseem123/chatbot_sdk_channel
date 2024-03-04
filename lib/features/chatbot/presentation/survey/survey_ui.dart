1c495ed807e88daf2263828a8f81b668672860ae
naseemzyngl <naseem@zartek.in>
2024-03-04 22:18:11 +0530

    Version 1.10.9

--- lib/features/chatbot/presentation/survey/survey_ui.dart
+++ lib/features/chatbot/presentation/survey/survey_ui.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:go_router/go_router.dart';
@@ -28,19 +26,22 @@ class _SurveyUiState extends State<SurveyUi> {
   Widget build(BuildContext context) {
     final surveyData = Uri.encodeComponent(jsonEncode(widget.surveyData));
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(),
       body: SafeArea(
         child: InAppWebView(
+            initialOptions: InAppWebViewGroupOptions(
+                crossPlatform: InAppWebViewOptions(
+                    transparentBackground: true)),
           initialUrlRequest: URLRequest(
               url: WebUri.uri(Uri.parse(
                   'https://test.ca.digital-front-door.stg.gcp.trchq.com/package_iframe/surveys?data=$surveyData'))),
+
           onWebViewCreated: (InAppWebViewController controller) {
             _webViewController = controller;
             _webViewController?.addJavaScriptHandler(
               handlerName: 'onMessageReceived',
               callback: (result) {
-                log('Message received from JavaScript: $result');
-
                 surveyMap = result[0];
                 Future.delayed(const Duration(milliseconds: 600)).then((value) {
                   context.pop(surveyMap);
@@ -52,8 +53,6 @@ class _SurveyUiState extends State<SurveyUi> {
           },
           onLoadStop: (controller, url) async {
             // Inject JavaScript code to set up a listener for post messages
-
-            print("load stop called");
             await controller.evaluateJavascript(source: '''
           window.addEventListener('message', function(event) {
            window.flutter_inappwebview.callHandler('onMessageReceived', event.data);
